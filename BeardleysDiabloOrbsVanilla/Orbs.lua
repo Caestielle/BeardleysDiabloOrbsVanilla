b88860428268881b2487005e297ad1e73052fd56
Caestielle <109084116+Caestielle@users.noreply.github.com>
2022-10-15 00:57:17 +0100

    Add files via upload

--- BeardleysDiabloOrbsVanilla/Orbs.lua
+++ BeardleysDiabloOrbsVanilla/Orbs.lua
@@ -2,15 +2,14 @@ local counter = 0
 local orb = CreateFrame('FRAME')
 local timeElapsed = 0
 orb:SetScript("OnUpdate", function()
-	if UnitAffectingCombat("Player") then
-		timeElapsed = timeElapsed + 1
-		if timeElapsed > 1 then
-			timeElapsed = 0
-			BDOMod_RedOrb:SetTexture("Interface\\Addons\\BeardleysDiabloOrbsVanilla\\d3orbanimation\\d3_orb_ani_frame"..counter..".tga")
-			counter = counter+1
-			if counter > 24 then
-				counter = 0
-			end
+	timeElapsed = timeElapsed + 1
+	if timeElapsed > 2 then
+		timeElapsed = 0
+		BDOMod_RedOrb:SetTexture("Interface\\Addons\\BeardleysDiabloOrbsVanilla\\d3orbanimation\\d3_orb_ani_frame"..counter..".tga")
+		BDOMod_BlueOrb:SetTexture("Interface\\Addons\\BeardleysDiabloOrbsVanilla\\d3orbanimation\\d3_orb_ani_frame"..counter..".tga")
+		counter = counter+1
+		if counter > 24 then
+			counter = 0
 		end
 	end
 end)
